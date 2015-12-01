6e6d717cba20ca711c994c383625d881ab6f66ae
Atli Gudlaugsson <atligud13@ru.is>
2015-12-01 12:35:00 +0000

    deployment script ready

--- deploymentscript.sh
+++ deploymentscript.sh
@@ -1,9 +1,5 @@
+echo Pushing docker image
 docker push atligud13/tictactoe
-ssh vagrant@192.168.33.10 << EOF
-    docker pull atligud13/tictactoe
-    dID=`docker ps -q`
-    docker  kill $dID
-    docker rm $dID
-    docker run -p 8080:8080 -d -e "NODE_ENV=production" atligud13/tictactoe
-    bash -l
-EOF
+
+echo Updating docker container
+ssh vagrant@192.168.33.10 'bash -s' < testmachinebash.sh
--- testmachinebash.sh
+++ testmachinebash.sh
@@ -1,5 +1,10 @@
+echo pulling docker immage
 docker pull atligud13/tictactoe
 dID=`docker ps -q`
+
+echo killing and removing previous docker image
 docker  kill $dID
 docker rm $dID
+
+echo running updated version
 docker run -p 8080:8080 -d -e "NODE_ENV=production" atligud13/tictactoe
