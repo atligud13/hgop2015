2e33f5013d68847efff6709f07490bc1c007070c
Atli <atligud13@ru.is>
2015-12-10 12:44:44 +0000

    load tests should be ready, starting with only 100 games

--- server/capacity/tictactoe.load.js
+++ server/capacity/tictactoe.load.js
@@ -3,7 +3,7 @@ var given = require('../fluid-api/tictactoeFluid').given;
 
 it('Should play 1000 games in x seconds.', function (done) {
   var doneCount = 0;
-  var gamesToPlay = 1000;
+  var gamesToPlay = 100;
   var x = 6;
 
   this.timeout(x * 1000);
@@ -16,9 +16,16 @@ it('Should play 1000 games in x seconds.', function (done) {
 
   for (var gameId = 0; gameId < gamesToPlay; gameId++) {
     given(user("TestUserOne").createsGame("" + gameId))
-
-      ...play game to end...
-
-      .expect("GameCreated").isOk(QED);
+    .and(user("OtherUser").joinsGame("" + gameId))
+    .and(user("YourUser").placesMove(0,0, "X"))
+    .and(user("OtherUser").placesMove(1,0, "O"))
+    .and(user("YourUser").placesMove(2,0, "X"))
+    .and(user("OtherUser").placesMove(0,1, "O"))
+    .and(user("YourUser").placesMove(2,1, "X"))
+    .and(user("OtherUser").placesMove(1,1, "O"))
+    .and(user("YourUser").placesMove(1,2, "X"))
+    .and(user("OtherUser").placesMove(2,2, "O"))
+    .and(user("YourUser").placesMove(0,2, "X"))
+    .expect("GameDraw").byUser("YourUser").isOk(done);
   }
 });
