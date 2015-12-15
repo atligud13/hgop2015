a410390db5834c597430ae31d80bf4c693888487
Atli <atligud13@ru.is>
2015-12-15 10:51:32 +0000

    more fixes

--- deploymentscript.sh
+++ deploymentscript.sh
@@ -1,2 +1,2 @@
 echo Updating docker container
-ssh vagrant@192.168.33.10 'bash -s' < testmachinebash.sh $1
+ssh vagrant@192.168.33.10 'bash -s' < testmachinebash.sh $1 $2
--- testmachinebash.sh
+++ testmachinebash.sh
@@ -5,4 +5,6 @@ echo killing and removing previous docker image
 docker  kill tictactoe:$1
 docker rm tictactoe:$1
 echo running updated version
+echo $1
+echo $2
 docker run -p $1:8080 -d --name tictactoe$1 -e "NODE_ENV=production" atligud13/tictactoe:$2
