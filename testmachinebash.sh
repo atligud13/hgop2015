cb3944bda1b2e26dcfc7c3daef213fe70db58d49
Atli <atligud13@ru.is>
2015-12-15 12:02:58 +0000

    more deployment script fixes

--- testmachinebash.sh
+++ testmachinebash.sh
@@ -2,8 +2,8 @@ echo pulling docker immage
 docker pull atligud13/tictactoe:$2
 
 echo killing and removing previous docker image
-docker  kill tictactoe:$1
-docker rm tictactoe:$1
+docker  kill tictactoe$1
+docker rm tictactoe$1
 echo running updated version
 echo $1
 echo $2
